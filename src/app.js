ad70f0eb44c94298ea4cb9c2fa9afc0037db6944
robmyers82 <robert_myers_@hotmail.com>
2018-03-22 00:05:30 -0300

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+class FreeAgentTracker extends React.Component {
+    
+    render() {
+        return (
+            <div>
+                <p>Let's start the code here!</p>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<FreeAgentTracker />, document.getElementById('app'));
