1f2769a2974211f1741def63f51c6200262b6ca7
Joe Tong <joetong89@icloud.com>
2019-03-13 15:11:01 -0300

    practice in class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,31 +8,281 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var FreeAgentTracker = function (_React$Component) {
-    _inherits(FreeAgentTracker, _React$Component);
+// class Header extends React.Component {
 
-    function FreeAgentTracker() {
-        _classCallCheck(this, FreeAgentTracker);
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>{ this.props.title} </h1>
+// 				{ this.props.children }
+// 			</div>
+// 		);
+// 	}
+// }
 
-        return _possibleConstructorReturn(this, (FreeAgentTracker.__proto__ || Object.getPrototypeOf(FreeAgentTracker)).apply(this, arguments));
-    }
+var Header = function Header(props) {
+	return React.createElement(
+		"div",
+		null,
+		React.createElement(
+			"h1",
+			null,
+			props.title
+		)
+	);
+};
 
-    _createClass(FreeAgentTracker, [{
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'div',
-                null,
-                React.createElement(
-                    'p',
-                    null,
-                    'Let\'s start the code here!'
-                )
-            );
-        }
-    }]);
+Header.defaultProps = {
+	title: "default title"
+};
 
-    return FreeAgentTracker;
+var Body = function (_React$Component) {
+	_inherits(Body, _React$Component);
+
+	function Body(props) {
+		_classCallCheck(this, Body);
+
+		var _this = _possibleConstructorReturn(this, (Body.__proto__ || Object.getPrototypeOf(Body)).call(this, props));
+
+		_this.saySomething = _this.saySomething.bind(_this);
+		_this.onSubmitForm = _this.onSubmitForm.bind(_this);
+
+		_this.state = {
+			message: "Woohoo!",
+			blah: "blah, blaj",
+			players: []
+		};
+		return _this;
+	}
+
+	_createClass(Body, [{
+		key: "onSubmitForm",
+		value: function onSubmitForm(e) {
+			e.preventDefault();
+
+			var newPlayer = {
+				name: e.target.elements.name.value,
+				age: e.target.elements.age.value,
+				skillLevel: e.target.elements.skillLevel.value
+			};
+
+			this.props.handleAddPlayer(newPlayer);
+		}
+	}, {
+		key: "saySomething",
+		value: function saySomething() {
+			var _this2 = this;
+
+			alert(this.props.welcomeMessage);
+
+			this.setState(function (prevState) {
+				return {
+					message: _this2.props.welcomeMessage
+				};
+			});
+		}
+	}, {
+		key: "renderPlayers",
+		value: function renderPlayers() {
+			var _this3 = this;
+
+			return this.props.players.map(function (player) {
+				return React.createElement(
+					"div",
+					{ key: player.name },
+					React.createElement(
+						"h1",
+						null,
+						player.name
+					),
+					React.createElement(
+						"h2",
+						null,
+						"Skill Level: ",
+						player.skillLevel
+					),
+					React.createElement(
+						"p",
+						null,
+						"Age: ",
+						player.age
+					),
+					React.createElement(
+						"button",
+						{ type: "button", onClick: function onClick() {
+								_this3.props.handleDeletePlayer(player);
+							} },
+						"Delete"
+					)
+				);
+			});
+		}
+	}, {
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"div",
+				null,
+				React.createElement(
+					"p",
+					null,
+					"This is the body. "
+				),
+				React.createElement(
+					"button",
+					{ type: "button", onClick: this.saySomething },
+					" Say Something"
+				),
+				React.createElement(
+					"h5",
+					null,
+					this.state.message
+				),
+				this.props.players.length <= 0 && React.createElement(
+					"p",
+					null,
+					"No players are available"
+				),
+				this.props.players.length >= 0 && React.createElement(
+					"div",
+					null,
+					this.renderPlayers()
+				),
+				React.createElement(
+					"button",
+					{ type: "button", onClick: this.props.handleDeleteAllPlayer },
+					"Delete All Players"
+				),
+				React.createElement(
+					"form",
+					{ onSubmit: this.onSubmitForm },
+					React.createElement(
+						"label",
+						null,
+						"Name:"
+					),
+					React.createElement("input", { type: "text", name: "name", placehoder: "Name" }),
+					" ",
+					React.createElement("br", null),
+					React.createElement(
+						"label",
+						null,
+						"Age:"
+					),
+					React.createElement("input", { type: "text", name: "age", placehoder: "Age" }),
+					" ",
+					React.createElement("br", null),
+					React.createElement(
+						"label",
+						null,
+						"Skill Level:"
+					),
+					React.createElement("input", { type: "text", name: "skillLevel", placeholder: "Skill Level" }),
+					React.createElement("br", null),
+					React.createElement("input", { type: "submit", value: "Submit" })
+				)
+			);
+		}
+	}]);
+
+	return Body;
+}(React.Component);
+
+var FreeAgentTracker = function (_React$Component2) {
+	_inherits(FreeAgentTracker, _React$Component2);
+
+	function FreeAgentTracker(props) {
+		_classCallCheck(this, FreeAgentTracker);
+
+		var _this4 = _possibleConstructorReturn(this, (FreeAgentTracker.__proto__ || Object.getPrototypeOf(FreeAgentTracker)).call(this, props));
+
+		_this4.handleAddPlayer = _this4.handleAddPlayer.bind(_this4);
+		_this4.handleDeletePlayer = _this4.handleDeletePlayer.bind(_this4);
+		_this4.handleDeleteAllPlayer = _this4.handleDeleteAllPlayer.bind(_this4);
+
+		_this4.state = {
+			players: []
+		};
+		return _this4;
+	}
+
+	_createClass(FreeAgentTracker, [{
+		key: "componentDidMount",
+		value: function componentDidMount() {
+			var players = JSON.parse(localStorage.getItem('freeagentplayers'));
+			if (players) {
+				this.setState(function (prevState) {
+					return {
+						players: players
+					};
+				});
+			}
+		}
+	}, {
+		key: "componentDidUpdate",
+		value: function componentDidUpdate(prevProps, prevState) {
+			localStorage.setItem('freeagentplayers', JSON.stringify(this.state.players));
+		}
+	}, {
+		key: "handleAddPlayer",
+		value: function handleAddPlayer(player) {
+			if (!player.name) {
+				return "Please enter a player name";
+			}
+
+			this.setState(function (prevState) {
+				return {
+					players: prevState.players.concat(player)
+				};
+			});
+		}
+	}, {
+		key: "handleDeletePlayer",
+		value: function handleDeletePlayer(playerToDelete) {
+			this.setState(function (prevState) {
+				return {
+					players: prevState.players.filter(function (player) {
+						return !(player.name == playerToDelete.name && player.skillLevel == playerToDelete.skillLevel && player.age == playerToDelete.age);
+					})
+				};
+			});
+		}
+	}, {
+		key: "handleDeleteAllPlayer",
+		value: function handleDeleteAllPlayer() {
+			this.setState(function (prevState) {
+				return {
+					players: []
+				};
+			});
+		}
+	}, {
+		key: "render",
+		value: function render() {
+			return React.createElement(
+				"div",
+				null,
+				React.createElement(
+					Header,
+					{ title: "This is the title" },
+					React.createElement(
+						"h2",
+						null,
+						"This is from children"
+					)
+				),
+				React.createElement(Body, {
+					welcomeMessage: "Hey There!!!!!",
+					players: this.state.players,
+					handleAddPlayer: this.handleAddPlayer,
+					handleDeletePlayer: this.handleDeletePlayer,
+					handleDeleteAllPlayer: this.handleDeleteAllPlayer
+				})
+			);
+		}
+	}]);
+
+	return FreeAgentTracker;
 }(React.Component);
 
 ReactDOM.render(React.createElement(FreeAgentTracker, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,9 +1,174 @@
+// class Header extends React.Component {
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>{ this.props.title} </h1>
+// 				{ this.props.children }
+// 			</div>
+// 		);
+// 	}
+// }
+
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.title}</h1>
+		</div>
+	);
+};
+
+Header.defaultProps = {
+	title: "default title"
+}
+
+class Body extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.saySomething = this.saySomething.bind(this);
+		this.onSubmitForm = this.onSubmitForm.bind(this);
+
+		this.state={
+			message: "Woohoo!",
+			blah: "blah, blaj",
+			players:[],
+		};
+	}
+
+	onSubmitForm(e) {
+		e.preventDefault();
+
+		const newPlayer = {
+			name: e.target.elements.name.value,
+			age: e.target.elements.age.value,
+			skillLevel: e.target.elements.skillLevel.value,
+		}
+
+		this.props.handleAddPlayer(newPlayer);
+	}
+	
+	saySomething() {
+		alert(this.props.welcomeMessage);
+
+		this.setState((prevState) => {
+			return {
+				message: this.props.welcomeMessage,
+			}
+		});
+	}
+
+	renderPlayers() {
+		return this.props.players.map( (player) => 
+			<div key={player.name}>
+				<h1>{player.name}</h1>
+				<h2>Skill Level: {player.skillLevel}</h2>
+				<p>Age: {player.age}</p>
+				<button type="button" onClick={() => {this.props.handleDeletePlayer(player)}}>Delete</button>
+			</div> 
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<p>This is the body. </p>
+				<button type="button" onClick={this.saySomething}> Say Something</button>
+				<h5>{this.state.message}</h5>
+				{this.props.players.length <=0 && <p>No players are available</p>}
+				{this.props.players.length >=0 && <div>{this.renderPlayers()}</div>}
+				<button type="button" onClick={this.props.handleDeleteAllPlayer}>Delete All Players</button>
+
+				<form onSubmit={this.onSubmitForm}>
+					<label>Name:</label>
+					<input type="text" name="name" placehoder="Name"/> <br/>
+					<label>Age:</label>
+					<input type="text" name="age" placehoder="Age"/> <br/>
+					<label>Skill Level:</label>
+          			<input type="text" name="skillLevel" placeholder="Skill Level" /><br/>
+          			<input type="submit" value="Submit"/>
+				</form>
+			</div>
+			
+		);
+	}
+}
+
 class FreeAgentTracker extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.handleAddPlayer = this.handleAddPlayer.bind(this);
+		this.handleDeletePlayer = this.handleDeletePlayer.bind(this);
+		this.handleDeleteAllPlayer = this.handleDeleteAllPlayer.bind(this);
+
+		this.state = {
+			players: [],
+		};
+	}
+
+	componentDidMount() {
+		const players = JSON.parse(localStorage.getItem('freeagentplayers'));
+		if (players) {
+			this.setState( (prevState) => {
+				return {
+					players: players,
+				}
+			});
+		}
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		localStorage.setItem('freeagentplayers', JSON.stringify(this.state.players));
+	}
+
+	handleAddPlayer(player) {
+		if (!player.name) {
+			return "Please enter a player name";
+		}
+
+		this.setState((prevState) => {
+			return {
+				players: prevState.players.concat(player),
+			}
+		});
+	}
+
+	handleDeletePlayer(playerToDelete) {
+		this.setState( (prevState) => {
+			return {
+				players: prevState.players.filter( (player) => {
+					return !(player.name == playerToDelete.name 
+						&& player.skillLevel == playerToDelete.skillLevel
+						&& player.age == playerToDelete.age); 
+				}),
+			}
+		});
+	}
+
+	handleDeleteAllPlayer() {
+		this.setState( (prevState) => {
+			return {
+				players:[],
+			}
+		});
+	}
     
     render() {
         return (
             <div>
-                <p>Let's start the code here!</p>
+            	<Header title={"This is the title"}>
+            		<h2>This is from children</h2>
+            	</Header>
+            	<Body 
+            		welcomeMessage="Hey There!!!!!" 
+            		players={this.state.players} 
+            		handleAddPlayer={this.handleAddPlayer} 
+            		handleDeletePlayer={this.handleDeletePlayer}
+            		handleDeleteAllPlayer={this.handleDeleteAllPlayer}
+            	/>
             </div>
         );
     }
